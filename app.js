26bcef55dcaace22a78032341e48deb92122ff15
Dean Rumsby <dean@deanrumsby.com>
2022-04-17 13:21:44 +0100

    start working on checkWin method by implementing checkHorizWin function

--- app.js
+++ app.js
@@ -11,14 +11,51 @@ function Connect4() {
   }
 
   this.addCounter = function(counter, column) {
-    // return 0 for success, return 1 if column is full
-    for (let i = 0; i < numRows; i++) {
-      if (this[i][column] === null) {
-        this[i][column] = counter;
-        return 0;
+    // returns counter position if successful
+    // returns 1 if column is full
+    for (let row = 0; row < numRows; row++) {
+      if (this[row][column] === null) {
+        this[row][column] = counter;
+        return [row, column];
       }
     }
     return 1;
+  }
+
+  this.checkWin = function(i, j) {
+    // checks for win on all possible win lines for some given counter position (i, j)
+    // returns true if win
+    // returns false if no win
+    // returns 1 if provided with a null counter position
+
+    if (this[i][j] === null) {
+      return 1;
+    }
+
+    const counter = this[i][j];
+    
+    this.checkHorizWin = function() {
+      // checks for possible horizontal wins
+      // returns true if win
+      // returns false if no win
+      let connected = 0;
+      for (let x = j - 3; x < j + 4; x++) {
+        if (this[i][x] === counter) {
+         connected++;
+        } else {
+         connected = 0;
+        }
+        if (connected === 4) {
+          return true
+        }
+      }
+      return false
+    }
+    if (this.checkHorizWin()) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
 
