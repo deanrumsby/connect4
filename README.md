1a48b55c0264a6508922ccf9f6a0f9180131ae42
Dean Rumsby <dean@deanrumsby.com>
2022-04-24 08:19:16 +0100

    first working version of view.js set up on index2.html

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ A simple (and most likely over-engineered) connect4 clone.
 
 Built as a way to primarily practice:
     Object Oriented Programming skills in vanilla JavaScript.
+    Unit testing with Jest.
     Model-View-Controller design patterns.
     Using JSDoc to create documentation.
 
--- js/view.js
+++ js/view.js
@@ -1,6 +1,27 @@
 class View {
-  constructor() {}
+  constructor(numCols, numRows) {
+    // the root element
+    this.root = this.getElement('#connect4');
 
+    // the title of the app
+    this.title = this.createElement('h1', 'title');
+    this.title.innerText = "Connect4";
+
+    // the game's slots, in a 2D array
+    // populated by this.createBoard
+    this.slots = [];
+
+    // the visual game board
+    this.board = this.createBoard(numCols, numRows);
+
+    // results container
+    this.results = this.createElement('div', 'results');
+
+    // append to the root element
+    this.root.append(this.title, this.board, this.results);
+  }
+
+  // creates DOM element with optional class
   createElement(tag, className) {
     const element = document.createElement(tag);
     if (className) {
@@ -9,7 +30,44 @@ class View {
     return element;
   }
 
+  // retrieves an element from the DOM
   getElement(selector) {
     return document.querySelector(selector);
   }
+
+  // retrieves all elements from the DOM with specified selector
+  getAllElements(selector) {
+    return document.querySelectorAll(selector);
+  }
+
+  // creates and returns the game's board element
+  // populates this.slots 
+  createBoard(numCols, numRows) {
+    // creating the visual board
+    const board = this.createElement('div', 'board');
+    board.style.aspectRatio = `${numCols} / ${numRows}`;
+
+    // creating each column of the board
+    for (let j = 0; j < numCols; j++) {
+      this.slots[j] = [];
+      const column = this.createElement('div', 'board-col');
+
+      // append it to the grid
+      board.append(column);
+
+      // create each cell and slot within each column
+      for (let i = 0; i < numRows; i++) {
+        const cell = this.createElement('div', 'cell');
+        const slot = this.createElement('div', 'slot');
+        cell.append(slot);
+        column.append(cell);
+
+        // add the slot to the 2D slots array
+        this.slots[j][i] = slot;
+      }
+    }
+    return board;
+  }
 }
+
+const view = new View(7, 6);
