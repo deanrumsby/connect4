558895d5a7ff9160c3a010af5a9831326b931b92
Dean Rumsby <dean@deanrumsby.com>
2022-04-29 16:52:25 +0100

    update controller with jsdoc comments

--- js/controller.js
+++ js/controller.js
@@ -1,22 +1,51 @@
+/**
+ * Controller for our Connect4 app.
+ */
 class Controller {
+  /**
+   * @param {Object} model A Connect4 model object. 
+   * @param {Object} view A Connect4 view object.
+   */
   constructor(model, view) {
+    /**
+     * A Connect4 model object.
+     * @type {Object}
+     */
     this.model = model;
+
+    /**
+     * A Connect4 view object.
+     * @type {Object}
+     */
     this.view = view;
 
+    /* Binding our event handlers and callbacks */
     this.view.bindAddCounter(this.handleAddCounter);
     this.view.bindReset(this.handleReset);
     this.model.bindEndGame(this.endGame);
   }
 
+  /**
+   * Handles the adding of counters to a chosen column. 
+   * Adds a counter to the model and updates the correct slot in the view.
+   * Clears any currently displayed messages.
+   * @param {number} column The index of the column chosen. 
+   * @returns {undefined} Returns if no coordinates were given to update.
+   */
   handleAddCounter = (column) => {
+    /* Clears messages, adds counter to model and receives 
+     * the coordinates for the newly placed piece */
     this.view.clearMessage();
     const coordinates = this.model.addCounter(column);
     
+    /* If no coordinates, will ask the view to display a column full message */
     if (!coordinates) {
       this.view.displayMessage(3);
       return;
     }
 
+    /* Updates the slot given by the coordinates with the color
+     * of the current player */
     const currentPlayer = this.model.currentPlayer();
     const [player1, player2] = this.model.counters;
     
@@ -28,15 +57,25 @@ class Controller {
         this.view.updateSlot(coordinates, 'yellow');
         break;
     }
+    /* Signals the model to end the turn */
     this.model.endTurn();
   }
 
+  /**
+   * Handles the resetting of the view and model.
+   */
   handleReset = () => {
     this.view.reset();
     this.model.reset();
     this.view.gameOver = false;
   }
 
+  /**
+   * Updates the view when signalled by the model that the
+   *   game has ended.
+   * @param {string|undefined} winner The winner's counter, 
+   *   or undefined if the game is a draw. 
+   */
   endGame = (winner) => {
     const [player1, player2] = this.model.counters;
     switch (winner) {
@@ -50,6 +89,8 @@ class Controller {
         this.view.displayMessage(2);
         break;
     }
+    /* Timeout given so that the reset events aren't unintentionally
+     * fired by a game ending keypress */
     setTimeout(() => {
       this.view.gameOver = true;
     }, 200);
