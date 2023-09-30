5f8442e49d9568f2dafc4915413393c4b22ca3da
Dean Rumsby <dean@deanrumsby.com>
2023-09-30 13:26:31 +0100

    feat: add more tests

--- src/hooks/useConnect4.test.ts
+++ src/hooks/useConnect4.test.ts
@@ -1,11 +1,31 @@
 import { test, expect } from "vitest";
 import { renderHook } from "@testing-library/react";
 
-import useConnect4 from "./useConnect4";
+import useConnect4, { DEFAULTS } from "./useConnect4";
 
 /**
  * @vitest-environment jsdom
  */
+
+test("should return initial board state and values", () => {
+  const { result } = renderHook(() => useConnect4());
+
+  const { boardState, nextPlayer, winner, stringifyBoard } = result.current;
+  const { nextAvailableRows, lastMove } = boardState;
+
+  expect(stringifyBoard()).toEqual(
+    "xxxxxxx\n" +
+      "xxxxxxx\n" +
+      "xxxxxxx\n" +
+      "xxxxxxx\n" +
+      "xxxxxxx\n" +
+      "xxxxxxx",
+  );
+  expect(nextPlayer).toBe(0);
+  expect(winner).toBe(null);
+  expect(lastMove).toBe(null);
+  expect(nextAvailableRows).toEqual([5, 5, 5, 5, 5, 5, 5]);
+});
 
 test("player should change after each counter added", () => {
   const { result, rerender } = renderHook(() => useConnect4());
@@ -40,4 +60,26 @@ test("counters stack as expected", () => {
       "11xxxxx\n" +
       "001x0xx",
   );
+});
+
+test("should throw error if column does not exist", () => {
+  const { result } = renderHook(() => useConnect4());
+
+  const { dropCounter } = result.current;
+  expect(() => dropCounter(-1)).toThrow(/exist/);
+  expect(() => dropCounter(7)).toThrow(/exist/);
+});
+
+test("should throw error if column is full", () => {
+  const { result, rerender } = renderHook(() => useConnect4());
+
+  for (let i = 0; i < DEFAULTS.numberOfRows; i++) {
+    const { dropCounter } = result.current;
+
+    dropCounter(0);
+    rerender();
+  }
+
+  const { dropCounter } = result.current;
+  expect(() => dropCounter(0)).toThrow(/full/);
 });
