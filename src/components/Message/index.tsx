69d3567b816d4f97f026a81504515d0947fb9970
Dean Rumsby <dean@deanrumsby.com>
2023-11-12 11:42:16 +0000

    feat: show winner message

--- src/components/Message/index.tsx
+++ src/components/Message/index.tsx
@@ -2,9 +2,19 @@ import useConnect4Context from "@/hooks/useConnect4Context";
 import "./Message.css";
 
 function Message() {
-  const { error } = useConnect4Context();
-  const message = error ? error : null;
-  return <div className="message">{message}</div>;
+  const { winner, error } = useConnect4Context();
+
+  const determineMessage = () => {
+    if (winner !== null) {
+      return `Player ${winner} wins!`;
+    } else if (error) {
+      return error;
+    } else {
+      return null;
+    }
+  };
+
+  return <p className="message">{determineMessage()}</p>;
 }
 
 export default Message;
